ce49267be8f70de7162b278ce80af97e3fcbe80b
Simphiwe Mabaso <Simphiwe.Mabaso@hollywoodbets.net>
2022-06-09 08:16:16 +0200

    First push to a clean repo

---
+++ buy-and-sell/src/app/listing-data-form/listing-data-form.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { Listing } from '../types';
+
+@Component({
+  selector: 'app-listing-data-form',
+  templateUrl: './listing-data-form.component.html',
+  styleUrls: ['./listing-data-form.component.css']
+})
+export class ListingDataFormComponent implements OnInit {
+  @Input() buttonText: any;
+  @Input() currentName = '';
+  @Input() currentDescription = '';
+  @Input() currentPrice = '';
+  
+  id: string = '';
+  name: string = '';
+  description: string = '';
+  price: string = '';
+
+  @Output() onSubmit = new EventEmitter<Listing>();
+
+  constructor(
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this.name = this.currentName;
+    this.description = this.description;
+    this.price = this.currentPrice;
+  }
+
+  onButtonClicked(): void{
+    this.onSubmit.emit({
+      id: '',
+      name: this.name,
+      description: this.description,
+      price: Number(this.price),
+    });
+  }
+
+}
